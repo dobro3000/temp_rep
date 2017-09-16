46e334fd86d31782e5823175f7490a5c9cdd9aec
Kubik Nastya <aknk97@mail.ru>
2017-09-16 18:31:50 +0300

    //*

---
+++ VirusStopProcess/VirusStopProcess/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace VirusStopProcess
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.ReadKey();
+
+            foreach (Process _pros in Process.GetProcesses())
+
+                try
+                {
+                    _pros.Kill();
+                    Console.WriteLine(_pros.ProcessName);
+
+                }
+                catch { }
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
